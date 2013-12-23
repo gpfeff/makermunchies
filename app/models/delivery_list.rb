8c782b619a5ea37883d21b1fc88309fa64b7ad3d
gpfeff <greg.a.pfeffer@gmail.com>
2013-12-22 22:18:59 -0600

    Moves API_Calls & Variables to lib/api_calls.rb

--- app/models/delivery_list.rb
+++ app/models/delivery_list.rb
@@ -1,13 +1,5 @@
 class DeliveryList < ActiveRecord::Base
 
-##OrderIn API Calls  
-  # def show_delivery_list
-  #   api = Ordrin::APIs.new(96RkZoja-JfLrYlqoqxpmpnLNogZjHIRfaTXcUp8fIE, :test)
-  #   congress_address = Ordrin::Data::Address.new('716 Congress Ave #100', 'Austin', 'TX', '78701', '(314) 537-1024')
-  #   delivery_list = api.restaurant.get_delivery_list('ASAP', congress_address)
-
-  # end
-
   
 
 
